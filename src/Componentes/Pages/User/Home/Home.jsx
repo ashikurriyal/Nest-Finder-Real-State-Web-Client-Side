8f50f78448d984df63639da2b4d1628e67f8548d
ashikurriyal <riyal.ashikur@gmail.com>
2024-06-05 04:09:59 +0600

    Advertisement Section implemented

--- src/Componentes/Pages/User/Home/Home.jsx
+++ src/Componentes/Pages/User/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className="space-y-20">
             <Banner></Banner>
-            <div className="lg:mx-32 mx-6 bg-red-300">
+            <div className="lg:mx-32 mx-6 ">
                 <AdvertisementSection></AdvertisementSection>
             </div>
         </div>
