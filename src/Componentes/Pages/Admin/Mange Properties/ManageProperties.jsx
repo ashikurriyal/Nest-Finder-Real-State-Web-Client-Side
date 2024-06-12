6cf64f3e87bbfa4bd92a9a6867bc9f7a4d9f4e0f
ashikurriyal <riyal.ashikur@gmail.com>
2024-06-12 14:54:50 +0600

    Loaded the all properties data from property collection to admin side Manage Properties page

--- src/Componentes/Pages/Admin/Mange Properties/ManageProperties.jsx
+++ src/Componentes/Pages/Admin/Mange Properties/ManageProperties.jsx
@@ -1,6 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
 
 
 const ManageProperties = () => {
+
+    const axiosSecure = useAxiosSecure();
+    const {data: properties = [] } = useQuery({
+        queryKey: ['properties'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/allProperties')
+            return res.data
+        }
+    })
+
+    console.log(properties)
     return (
         <div>
             Manage Properties
